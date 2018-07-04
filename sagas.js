499dcd0d7ee077b88ed427d09d1f853643f9ce6b
joecfan <48155251@qq.com>
2018-07-04 17:53:52 +0800

    init

---
+++ sagas.js
@@ -0,0 +1,28 @@
+import { delay } from 'redux-saga'
+import { put, takeEvery,all } from 'redux-saga/effects'
+
+  function* helloSaga() {
+  console.log('Hello Sagas!');
+}
+
+// Our worker Saga: will perform the async increment task
+ function* incrementAsync() {
+	console.log('incrementAsync ...')
+  yield delay(1000)
+  
+  yield put({ type: 'INCREMENT' })
+}
+
+// Our watcher Saga: spawn a new incrementAsync task on each INCREMENT_ASYNC
+ function* watchIncrementAsync() {
+  yield takeEvery('INCREMENT_ASYNC', incrementAsync)
+}
+
+// notice how we now only export the rootSaga
+// single entry point to start all Sagas at once
+ export function* rootSaga() {
+  yield all([
+    helloSaga(),
+    watchIncrementAsync()
+  ])
+}
